2e521fd89ed24515140aba8c8f9f0b0530eb5501
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-29 08:57:42 -0700

    liveserve is working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Header from './Header.js';
 import Nav from './Nav.js';
-import Search from './Search.js';
+//import PokeList from './PokeList';
 
 function App() {
   return (
     <div className="App">
       <Header />
         <Nav />
-        <Search />
     </div>
   );
 }
