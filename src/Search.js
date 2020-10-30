1228d7151a0f3ba022c652bd4988f13ae1e83103
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-10-29 17:37:24 -0700

    added handleTyping and place holder text to input

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
 
 export default class Search extends Component {
+
+    state = {
+        input: ""
+    }
+
+    handleTyping = (e) => {
+        this.setState({input: e.target.value})
+        let text = e.target.value;
+            text = text.toLowerCase()
+            .split(' ')
+            .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(' ')
+        this.props.filterWorker(text)
+    }
+
     render() {
         return (
-            <div>
-                <form onsubmit={this.props.handleSearch}>
-                    <input value={this.props.searchQuery} onChange={this.props.handleChange} />
+            <div className="search">
+                <form onSubmit={this.props.handleSearch}>
+                    <input type="text" placeholder="Search by pokemon name..." value={this.props.searchQuery} onChange={this.props.handleChange} />
                     <button>Search</button>
                 </form>
             </div>
